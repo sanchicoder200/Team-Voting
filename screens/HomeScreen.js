32d803c4fb16b014352bbf4b0aac29b700664424
sanchicoder200 <82163770+sanchicoder200@users.noreply.github.com>
2022-01-03 17:29:57 +0530

    Update HomeScreen.js
    changing background color to purple

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"purple",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
